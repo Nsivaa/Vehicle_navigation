02c14106d095dbc96b6b7f7291a70ca4c697a577
Nsivaa <alberto.dicembre@gmail.com>
2024-06-15 23:45:05 +0200

    added cv image reader

--- ROS_Plugins/catkin_ws/src/navigation/src/cam_subscriber.py
+++ ROS_Plugins/catkin_ws/src/navigation/src/cam_subscriber.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 import rospy
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id () + f"Received {data.data}")
+class CameraProcessor:
+    
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.sub = rospy.Subscriber("/husky_model/husky/camera", Image, self.callback, queue_size=1)
+
+    def callback(self, data):
+        # rospy.loginfo(rospy.get_caller_id() + f"Received {data.data}")
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+        except CvBridgeError as e:
+            print(e)
+        cv2.imshow("Image window", cv_image)
+        cv2.waitKey(3)
 
 def listener():
+    cp = CameraProcessor()
     rospy.init_node('cam_subscriber', anonymous = True)
-    rospy.Subscriber("/husky_model/husky/camera", Image, callback)
     rospy.spin()
 
 if __name__ == '__main__':
