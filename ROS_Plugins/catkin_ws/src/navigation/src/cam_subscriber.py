a63933e30b822685e7afac59f878854b3ad39885
Nsivaa <alberto.dicembre@gmail.com>
2024-06-15 23:03:30 +0200

    changed data type to image

--- ROS_Plugins/catkin_ws/src/navigation/src/cam_subscriber.py
+++ ROS_Plugins/catkin_ws/src/navigation/src/cam_subscriber.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import rospy
-from std_msgs.msg import String
+from sensor_msgs.msg import Image
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id () + f"Received {data.data}")
 
 def listener():
     rospy.init_node('cam_subscriber', anonymous = True)
-    rospy.Subscriber("/husky_model/husky/camera", String, callback)
+    rospy.Subscriber("/husky_model/husky/camera", Image, callback)
     rospy.spin()
 
 if __name__ == '__main__':
